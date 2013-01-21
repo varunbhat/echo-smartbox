822ba67eccdd3c99c7ed2a95e73c729104e42613
varunbhat <varunbhat.kn@gmail.com>
2013-01-22 04:05:11 +0530

    partly working eeprom library

--- Dfygraviti/dfygraviti.cpp
+++ Dfygraviti/dfygraviti.cpp
@@ -16,6 +16,7 @@ void DfygravitiServer::initialize() {
 	else
 		rtc_enabled = true;
 	irrecv.enableIRIn();
+	
 }
 
 //Set the RTC for the present time.. needs to be invoked for maintainance
@@ -73,7 +74,12 @@ void DfygravitiServer::setTvChannel() {
 
 void DfygravitiServer::irRemotebuttonPressPoll() {
 	if (irrecv.decode(&results)) {
-		Serial.println(results.value, HEX);
+		if (results.decode_type != UNKNOWN) {
+
+			DateTime now = RTC.now();
+			Serial.println(results.value, HEX);
+			DBman.push(now.unixtime(), results.value);
+		}
 		irrecv.resume();
 	}
 }
@@ -89,9 +95,7 @@ char * DfygravitiServer::getSerialData(int byteLen) {
 	return buffer;
 }
 
-//IRUserDatabase::IRUserDatabase() {
-//_id =
-//}
+//////////////////////////////////////////////////Database Manager////////////////////////
 
 DatabaseManager::DatabaseManager() {
 	byte programCode = EEPROM.read(0);
@@ -100,32 +104,40 @@ DatabaseManager::DatabaseManager() {
 }
 
 bool DatabaseManager::initializeDB() {
-	RTC.begin();
-	DateTime now = RTC.now();
-	EEPROM.write(0, DatabaseString);
-	pushlastUpdateTime(now.unixtime());
-	pushlastDumpTime(now.unixtime());
+	pushToEEPROM(0, 1, DatabaseString);
+	pushlastUpdateTime(1234567);
+	pushlastDumpTime(1234567);
 	setEEPROMLocation (INTERNAL);
 	stackEntries(0);
 	stackStartAddress(13);
+	stackpointer(13);
+}
+///////////////////////////////////////////////////////stack pointer set & retrive//////
+bool DatabaseManager::stackpointer(uint8_t address) {
+	return pushToEEPROM(12, 1, address);
 }
 
-bool DatabaseManager::stackStartAddress(uint8_t address){
-	return pushToEEPROM(11,1,address)
+uint8_t DatabaseManager::stackpointer() {
+	return getFromEEPROM(13, 1);
 }
 
-uint8_t DatabaseManager::stackStartAddress(){
+///////////////////////////////////////////////////////// Set stack start address///////
+bool DatabaseManager::stackStartAddress(uint8_t address) {
+	return pushToEEPROM(11, 1, address);
+}
+uint8_t DatabaseManager::stackStartAddress() {
 	return getFromEEPROM(11, 1);
 }
 
+////////////////////////////////////////////////////////// count of the total number of entries///
 bool DatabaseManager::stackEntries(uint8_t number) {
 	return pushToEEPROM(10, 1, number);
 }
-
 uint8_t DatabaseManager::stackEntries() {
 	return getFromEEPROM(10, 1);
 }
 
+////////////////////////////////////////////////////////////////Pushing and getting to eeprom main Func//////
 // Little endian storage (LSB = Lower memory location)
 uint32_t DatabaseManager::getFromEEPROM(uint8_t start, uint8_t bytes) {
 	// make changes here for xternal eeprom
@@ -150,33 +162,73 @@ bool DatabaseManager::pushToEEPROM(uint8_t start, uint8_t bytes,
 	}
 	return true;
 }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/////////////////////////////////////////////////////////// last updated time /////////////
 uint32_t DatabaseManager::getlastUpdateTime() {
 	return getFromEEPROM(1, 4);
-}
-
-uint32_t DatabaseManager::getlastDumpTime() {
-	return getFromEEPROM(5, 4);
 }
 
 bool DatabaseManager::pushlastUpdateTime(uint32_t mytime) {
 	return pushToEEPROM(1, 4, mytime);
 }
 
+/////////////////////////////////////////////////////////////last Dump time //////////////
+
+uint32_t DatabaseManager::getlastDumpTime() {
+	return getFromEEPROM(5, 4);
+}
+
 bool DatabaseManager::pushlastDumpTime(uint32_t mytime) {
 	return pushToEEPROM(5, 4, mytime);
 }
 
+/////////////////////////////////////////////////////////////Set external or internal eeprom/////////
 bool DatabaseManager::getEEPROMLocation() {
 	return getFromEEPROM(9, 1) & 0x01;
 }
 
 bool DatabaseManager::setEEPROMLocation(bool type) {
-	uint8_t temp = getFromEEPROM(6, 1) | (type * 0x01);
-	return pushToEEPROM(6, 1, temp);
+	uint8_t temp = getFromEEPROM(9, 1) | (type * 0x01);
+	return pushToEEPROM(9, 1, temp);
+}
+void DatabaseManager::incStackCount() {
+	uint8_t temp = getFromEEPROM(10, 1);
+	pushToEEPROM(10, 1, temp + 1);
 }
 
-uint8_t DatabaseManager::push(IRUserDatabase db) {
-	pushToEEPROM()
+///////////////////////////////////////////////////////////////////Housekeeping to get no of bytes////
+uint8_t DatabaseManager::getByteLen(uint32_t number) {
+	uint8_t count = 1;
+	while (number) {
+		number >>= 8;
+		if (number != 0)
+			count++;
+	}
+	return count;
+}
+/////////////////////////////////////////////////////////////////////push values to the database ------------
+uint8_t DatabaseManager::push(uint32_t receiption_time, uint32_t key) {
+	uint32_t dTime = receiption_time - getlastDumpTime();
+	uint8_t dtimeLen = getByteLen(dTime);
+	uint8_t keylen = getByteLen(key);
+	uint8_t sp = stackpointer();
+	uint8_t status;
+	if ((dTime <= 1 && key == -1) || dTime <= 1 && key == lastkey) {
+		pushToEEPROM(sp - 1, 1, getFromEEPROM(sp - 1, 1) + 1);
+		return 0;
+	}
+	status = ((dtimeLen - 1) << 6) | ((keylen - 1) << 4)
+			| (getFromEEPROM(sp, 1) & 0x00ff);
+	pushToEEPROM(sp, 1, status);
+	pushToEEPROM(sp + 1, dtimeLen, dTime);
+	pushToEEPROM(sp + dtimeLen + 1, keylen, key);
+	pushToEEPROM(sp + dtimeLen + keylen + 1, 1, 1);
+	stackpointer(sp + dtimeLen + keylen + 1 + 1);
+	pushToEEPROM(sp + dtimeLen + keylen + 1 + 1, 1,
+			(sp + dtimeLen + keylen + 1 + 1) & 0xff);
+	incStackCount();
+	lastkey = key;
+	return sp + dtimeLen + keylen + 1 + 1;
 }
 
--- Dfygraviti/dfygraviti.h
+++ Dfygraviti/dfygraviti.h
@@ -10,6 +10,48 @@
 #include <RTClib.h>
 #include <Arduino.h>
 
+class IRUserDatabase {
+public:
+	IRUserDatabase();
+	//database elements
+	uint32_t receiptionTime;	// What time the key is pressed
+	uint32_t key; 				// Which key is pressed
+	uint8_t pressedCycles;  // How long it is pressed
+	uint8_t statusRegister;
+};
+
+class DatabaseManager {
+public:
+	DatabaseManager();
+	bool initializeDB();
+	uint32_t getlastDumpTime();
+	uint32_t getlastUpdateTime();
+	bool pushlastDumpTime(uint32_t mytime);
+	bool pushlastUpdateTime(uint32_t mytime);
+	bool getEEPROMLocation();
+	bool setEEPROMLocation(bool type);
+	uint8_t stackEntries(void);
+	bool stackEntries(uint8_t);
+	uint8_t push(uint32_t receiption_time, uint32_t key);
+	bool stackStartAddress(uint8_t address);
+	uint8_t stackStartAddress(void);
+	bool stackpointer(uint8_t address);
+	uint8_t stackpointer(void);
+	uint32_t lastkey;
+	uint8_t lastLoc;
+
+private:
+	RTC_DS1307 RTC;
+	uint32_t lastUpdateTime;
+	uint32_t lastDumpTime;
+	uint8_t stackEntrys;
+	uint32_t getFromEEPROM(uint8_t start, uint8_t bytes);
+	bool pushToEEPROM(uint8_t start, uint8_t bytes, uint32_t data);
+	uint8_t getByteLen(uint32_t);
+	void incStackCount();
+
+};
+
 class DfygravitiServer {
 public:
 	DfygravitiServer();
@@ -27,44 +69,6 @@ private:
 	IRsend irsend;
 	decode_results results;
 	char * getSerialData(int);
+	DatabaseManager DBman;
 };
-
-class IRUserDatabase {
-public:
-	IRUserDatabase();
-	//database elements
-	uint32_t receiptionTime;	// What time the key is pressed
-	uint32_t key; 				// Which key is pressed
-	uint8_t pressedCycles;  // How long it is pressed
-	uint8_t statusRegister;
-
-};
-
-class DatabaseManager {
-public:
-	DatabaseManager();
-	bool initializeDB();
-	uint32_t getlastDumpTime();
-	uint32_t getlastUpdateTime();
-	bool pushlastDumpTime(uint32_t mytime);
-	bool pushlastUpdateTime(uint32_t mytime);
-	bool getEEPROMLocation();
-	bool setEEPROMLocation(bool type);
-	uint8_t stackEntries(void);
-	bool stackEntries(uint8_t);
-	uint8_t push(IRUserDatabase db);
-	bool stackStartAddress(uint8_t address);
-	uint8_t stackStartAddress(void);
-private:
-	RTC_DS1307 RTC;
-	uint32_t lastUpdateTime;
-	uint32_t lastDumpTime;
-	uint8_t stackEntrys;
-	uint32_t getFromEEPROM(uint8_t start, uint8_t bytes);
-	bool pushToEEPROM(uint8_t start, uint8_t bytes, uint32_t data);
-
-};
-
-extern DatabaseManager DBman;
-
 #endif
