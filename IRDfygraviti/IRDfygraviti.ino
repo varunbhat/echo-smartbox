b2981720d73640e9aced9125165abeed8e202006
varunbhat <varunbhat.kn@gmail.com>
2013-01-24 04:18:15 +0530

    Readme updated. Ready to burn

--- IRDfygraviti/IRDfygraviti.ino
+++ IRDfygraviti/IRDfygraviti.ino
@@ -1,12 +1,13 @@
 #include <dfygraviti.h>
-
 DfygravitiServer echo;
+
+//Jan 23 201315:33:24
 
 void setup()
 {
   Serial.begin(9600);
   Serial.println("started");
-  echo.initialize();
+  echo.begin();
 
   pinMode(2,OUTPUT);
   pinMode(3,OUTPUT);
@@ -14,113 +15,53 @@ void setup()
   pinMode(13,OUTPUT);
   digitalWrite(13,HIGH);
   pinMode(11,INPUT);
+  digitalWrite(2,LOW);
+  pinMode(A3,OUTPUT);
+  digitalWrite(A3,HIGH);
+  pinMode(A2,OUTPUT);
+  digitalWrite(A2,LOW);
 
 }
 
 void loop()  
 {
-  if (Serial.available()>2)
+  if (Serial.available()>=2)
     switch (Serial.read())
     {
-      // case to Set the RTCtime.. !important
-      case 0x33: // 33a0
+    case 0x33: // 33a0
       if (Serial.read()==0xa0)
         echo.setRtcTime(); 
+    case 0x34: // 33a0
+      if (Serial.read()==0xa0)
+        echo.setRtcTime(1); 
       break;
-      // Set the channel on the tv. Sit back and enjoy the movie
+    case 0x19:
+      if (Serial.read()==0x91)
+        echo.pop(); 
+      break;
     case 0x38:
       if (Serial.read()==0x39)
         echo.setTvChannel();
       break;
-      // Get the time.. 
-    case 0x45:
+    case 0x45://4521
       if (Serial.read()==0x21)
-        echo.getRtcTime();  
+        echo.getRtcTime(); 
+      break; 
+    case 0x51: //514a
+      if (Serial.read()==0x4a)
+        echo.clearMemory();
+      break;
+    case 0xc3: //514a
+      if (Serial.read()==0xab)
+        echo.readRaw();
+      break;
     default:
-
       break;
     }
-    // poll for button press on the remote
   echo.irRemotebuttonPressPoll();
 }
 
-/////////// workaround for arduino i2c eeprom//////////
-//
-//
-//void i2c_eeprom_write_byte( int deviceaddress, unsigned int eeaddress, byte data ) {
-//  int rdata = data;
-//  Wire.beginTransmission(deviceaddress);
-//  Wire.send((int)(eeaddress >> 8)); // MSB
-//  Wire.send((int)(eeaddress & 0xFF)); // LSB
-//  Wire.send(rdata);
-//  Wire.endTransmission();
-//}
-//
-//// WARNING: address is a page address, 6-bit end will wrap around
-//// also, data can be maximum of about 30 bytes, because the Wire library has a buffer of 32 bytes
-//void i2c_eeprom_write_page( int deviceaddress, unsigned int eeaddresspage, byte* data, byte length ) {
-//  Wire.beginTransmission(deviceaddress);
-//  Wire.send((int)(eeaddresspage >> 8)); // MSB
-//  Wire.send((int)(eeaddresspage & 0xFF)); // LSB
-//  byte c;
-//  for ( c = 0; c < length; c++)
-//    Wire.send(data[c]);
-//  Wire.endTransmission();
-//}
-//
-//byte i2c_eeprom_read_byte( int deviceaddress, unsigned int eeaddress ) {
-//  byte rdata = 0xFF;
-//  Wire.beginTransmission(deviceaddress);
-//  Wire.send((int)(eeaddress >> 8)); // MSB
-//  Wire.send((int)(eeaddress & 0xFF)); // LSB
-//  Wire.endTransmission();
-//  Wire.requestFrom(deviceaddress,1);
-//  if (Wire.available()) rdata = Wire.receive();
-//  return rdata;
-//}
-//
-//// maybe let's not read more than 30 or 32 bytes at a time!
-//void i2c_eeprom_read_buffer( int deviceaddress, unsigned int eeaddress, byte *buffer, int length ) {
-//  Wire.beginTransmission(deviceaddress);
-//  Wire.send((int)(eeaddress >> 8)); // MSB
-//  Wire.send((int)(eeaddress & 0xFF)); // LSB
-//  Wire.endTransmission();
-//  Wire.requestFrom(deviceaddress,length);
-//  int c = 0;
-//  for ( c = 0; c < length; c++ )
-//    if (Wire.available()) buffer[c] = Wire.receive();
-//}
-//
-//
-//
-//
-//void setup() 
-//{
-//  char somedata[] = "this is data from the eeprom"; // data to write
-//  Wire.begin(); // initialise the connection
-//  Serial.begin(9600);
-//  i2c_eeprom_write_page(0x50, 0, (byte *)somedata, sizeof(somedata)); // write to EEPROM 
-//  delay(10); //add a small delay
-//  Serial.println("Memory written");
-//}
-//
-//void loop() 
-//{
-//  int addr=0; //first address
-//  byte b = i2c_eeprom_read_byte(0x50, 0); // access the first address from the memory
-//
-//  while (b!=0) 
-//  {
-//    Serial.print((char)b); //print content to serial port
-//    addr++; //increase address
-//    b = i2c_eeprom_read_byte(0x50, addr); //access an address from the memory
-//  }
-//  Serial.println(" ");
-//  delay(2000);
-//
-//}
-//
-//
-//
+
+
 
 
